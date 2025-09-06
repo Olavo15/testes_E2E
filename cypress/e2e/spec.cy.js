9b68ed8de70ebd58e62d62f8409ca6c094ace4de
olavo15 <olavoregis169@gmail.com>
2025-09-05 22:59:32 -0300

    FIX: Cadastro

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,15 @@
+const { usarname } = require("../fixtures/usarnames.ts");
+
 describe("template spec", () => {
   it("passes", () => {
     cy.visit("https://adopet-frontend-cypress.vercel.app/");
+    cy.contains("a", "Cadastrar").click();
+    cy.get("#name").type(usarname.name);
+    cy.get("input[name='email']").type(usarname.email);
+    cy.get("#pass-create").type(usarname.pwd);
+    // cy.get("span > .pass__view").first().click();
+    cy.get("#pass-confirm").type(usarname.new_pwd);
+    // cy.get("span > .pass__view").eq(1).click();
+    cy.contains("button", "Cadastrar").click();
   });
 });
---
+++ cypress/fixtures/usarnames.ts
@@ -0,0 +1,7 @@
+// Todos esses dados foram gerados no 4dev
+export const usarname = {
+  name: "João Lucas",
+  email: "joao-daconceicao79@email.com",
+  pwd: "n9R5brxrFW",
+  new_pwd: "n9R5brxrFW",
+};
