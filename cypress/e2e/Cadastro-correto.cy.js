33efe174c6a6b2ce64fb2244319c16aba5484c27
olavo15 <olavoregis169@gmail.com>
2025-09-06 13:26:52 -0300

    FIX: Modifying the correct registration

--- cypress/e2e/Cadastro-correto.cy.js
+++ cypress/e2e/Cadastro-correto.cy.js
@@ -3,13 +3,13 @@ const { usarname } = require("../fixtures/usarnames.ts");
 describe("Pagin cadastru", () => {
   it("You must fill in the form fields correctly to register a new user", () => {
     cy.visit("https://adopet-frontend-cypress.vercel.app/");
-    cy.contains("a", "Cadastrar").click();
-    cy.get("#name").type(usarname.name);
-    cy.get("input[name='email']").type(usarname.email);
-    cy.get("#pass-create").type(usarname.pwd);
+    cy.get("[data-test='register-button']").click();
+    cy.get("[data-test='input-name']").type(usarname.name);
+    cy.get("[data-test='input-email']").type(usarname.email);
+    cy.get("[data-test='input-password']").type(usarname.pwd);
     // cy.get("span > .pass__view").first().click();
-    cy.get("#pass-confirm").type(usarname.new_pwd);
+    cy.get("[data-test='input-confirm-password']").type(usarname.new_pwd);
     // cy.get("span > .pass__view").eq(1).click();
-    cy.contains("button", "Cadastrar").click();
+    cy.get("[data-test='submit-button']").click();
   });
 });
