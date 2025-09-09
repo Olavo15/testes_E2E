5d79d94a4867d23896d17b171a44b28d967950e1
UserName <Email@gmail.com>
2025-09-08 21:18:46 -0300

    FIX:Updating to use command

--- cypress/e2e/incorrectLogin.cy.js
+++ cypress/e2e/incorrectLogin.cy.js
@@ -4,12 +4,6 @@ describe("login page", () => {
     cy.get('[data-test="login-button"]').click();
   });
   it("Log incorrect", () => {
-    cy.get('[data-test="input-loginEmail"]').type("email.jjs@naosei");
-    cy.get('[data-test="input-loginPassword"]').type("La");
-    cy.get('[data-test="submit-button"]').click();
-    cy.contains("Por favor, verifique o email digitado").should("be.visible");
-    cy.contains(
-      "A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres"
-    ).should("be.visible");
+    cy.incorrectLogin("email.jjs@naosei", "La");
   });
 });
