ed8c604568dc07e7afdc9207869e5533733dd93e
UserName <Email@gmail.com>
2025-09-08 21:18:31 -0300

    FIX:Updating to use command

--- cypress/e2e/correctLogin.cy.js
+++ cypress/e2e/correctLogin.cy.js
@@ -6,8 +6,6 @@ describe("login page", () => {
     cy.get('[data-test="login-button"]').click();
   });
   it("Log in with the registered user", () => {
-    cy.get('[data-test="input-loginEmail"]').type(usarname.email);
-    cy.get('[data-test="input-loginPassword"]').type(usarname.pwd);
-    cy.get('[data-test="submit-button"]').click();
+    cy.login(usarname.email, usarname.pwd);
   });
 });
