0e42ed7f6fcf9ae463df48b05ae819d79d00b68a
UserName <Email@gmail.com>
2025-09-08 21:17:51 -0300

    FEAT:Creating commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,37 @@
+Cypress.Commands.add("login", (email, password) => {
+  cy.get('[data-test="input-loginEmail"]').type(email);
+  cy.get('[data-test="input-loginPassword"]').type(password);
+  cy.get('[data-test="submit-button"]').click();
+});
+
+Cypress.Commands.add("incorrectLogin", (email, password) => {
+  cy.get('[data-test="input-loginEmail"]').type(email);
+  cy.get('[data-test="input-loginPassword"]').type(password);
+  cy.get('[data-test="submit-button"]').click();
+  cy.contains("Por favor, verifique o email digitado").should("be.visible");
+  cy.contains(
+    "A senha deve conter pelo menos uma letra maiúscula, um número e ter entre 6 e 15 caracteres"
+  ).should("be.visible");
+});
+
+Cypress.Commands.add("register", (name, email, password, cfmpassword) => {
+  cy.get("[data-test='input-name']").type(name);
+  cy.get("[data-test='input-email']").type(email);
+  cy.get("[data-test='input-password']").type(password);
+  cy.get("[data-test='input-confirm-password']").type(cfmpassword);
+  cy.get("[data-test='submit-button']").click();
+});
+
+Cypress.Commands.add("incorrectRegister", () => {
+  cy.visit("https://adopet-frontend-cypress.vercel.app/");
+  cy.get("[data-test='register-button']").click("");
+  cy.get("[data-test='submit-button']").click();
+  cy.contains("É necessário informar um endereço de email").should(
+    "be.visible"
+  );
+  cy.contains("Crie uma senha").should("be.visible");
+  cy.contains("Repita a senha criada acima").should("be.visible");
+});
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
